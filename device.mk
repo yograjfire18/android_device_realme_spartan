599509bc5d82bdc82e272cc6a944e6d59a1d0530
yograjfire18 <yograjfire18@gmail.com>
2023-12-08 19:32:02 +0530

    spartan: Add removepackages * remove unwanted apps from final zip packages

--- device.mk
+++ device.mk
@@ -363,6 +363,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    removepackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
